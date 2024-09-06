e6622d1679dc9e267a4a1d4dd6ee731f8e910199
Shiva Krishna Beeraboina <shivakrishnabeeraboina@gmail.com>
2024-09-06 22:15:56 +0530

    Added Request methods, QueryStrings, Calculator using Query

---
+++ CalculatorOperationsRequests.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace ASPNetLearningCodes
+{
+    internal class CalculatorOperationsRequests
+    {
+        internal static void Caluclator(WebApplication app = null)
+        {
+
+            //Terminating middleware
+            app.Run(async (HttpContext context) =>
+            {
+                if (context.Request.Method == "POST" && context.Request.Path == "/")
+                {
+                    int firstNumber = 0, secondNumber = 0;
+                    string? operation = null;
+                    long? result = null;
+
+                    //read 'firstNumber' if submitted in the request body
+                    if (context.Request.Query.ContainsKey("Operand1"))
+                    {
+                        string firstNumberString = context.Request.Query["Operand1"][0];
+                        if (!string.IsNullOrEmpty(firstNumberString))
+                        {
+                            firstNumber = Convert.ToInt32(firstNumberString);
+                        }
+                        else
+                        {
+                            context.Response.StatusCode = 400;
+                            await context.Response.WriteAsync("Invalid input for 'Operand1'\n");
+                        }
+                    }
+                    else
+                    {
+                        if (context.Response.StatusCode == 200)
+                            context.Response.StatusCode = 400;
+                        await context.Response.WriteAsync("Invalid input for 'Operand1'\n");
+                    }
+
+                    //read 'secondNumber' if submitted in the request body
+                    if (context.Request.Query.ContainsKey("Operand2"))
+                    {
+                        string secondNumberString = context.Request.Query["Operand2"][0];
+                        if (!string.IsNullOrEmpty(secondNumberString))
+                        {
+                            secondNumber = Convert.ToInt32(context.Request.Query["Operand2"][0]);
+                        }
+                        else
+                        {
+                            if (context.Response.StatusCode == 200)
+                                context.Response.StatusCode = 400;
+                            await context.Response.WriteAsync("Invalid input for 'Operand2'\n");
+                        }
+                    }
+                    else
+                    {
+                        if (context.Response.StatusCode == 200)
+                            context.Response.StatusCode = 400;
+                        await context.Response.WriteAsync("Invalid input for 'Operand2'\n");
+                    }
+
+                    //read 'operation' if submitted in the request body
+                    if (context.Request.Query.ContainsKey("Operation"))
+                    {
+                        operation = Convert.ToString(context.Request.Query["Operation"][0]);
+
+                        //perform the calculation based on the value of "operation"
+                        switch (operation)
+                        {
+                            case "add": result = firstNumber + secondNumber; break;
+                            case "subtract": result = firstNumber - secondNumber; break;
+                            case "multiply": result = firstNumber * secondNumber; break;
+                            case "divide": result = (secondNumber != 0) ? firstNumber / secondNumber : 0; break; //avoid DivideByZeroException, if secondNuber is 0 (zero)
+                            case "mod": result = (secondNumber != 0) ? firstNumber % secondNumber : 0; break; //avoid DivideByZeroException, if secondNuber is 0 (zero)
+                        }
+
+                        //If no case matched above, the "result" remains as 'null'
+                        if (result.HasValue)
+                        {
+                            await context.Response.WriteAsync(result.Value.ToString());
+                        }
+
+                        //if invalid value is submitted for "operation" parameter
+                        else
+                        {
+                            if (context.Response.StatusCode == 200)
+                                context.Response.StatusCode = 400;
+                            await context.Response.WriteAsync("Invalid input for 'Operation'\n");
+                        }
+
+                    } //end of "of ContainsKey("operation")
+
+                    //if the "operation" parameter is submitted from the client
+                    else
+                    {
+                        if (context.Response.StatusCode == 200)
+                            context.Response.StatusCode = 400;
+                        await context.Response.WriteAsync("Invalid input for 'Operation'\n");
+                    }
+                }
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
+using ASPNetLearningCodes;
+using Microsoft.Extensions.Primitives;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+/*
 app.Run(async (HttpContext httpContext) =>
 {
+
     //httpContext.Response.StatusCode = 200;
 
     httpContext.Response.Headers["Content-type"] = "text/html";
     httpContext.Response.Headers["X-Powered-By"] = "ASP.NET Core";
     await httpContext.Response.WriteAsync("Hello World!");
-    await httpContext.Response.WriteAsync("<h1> Keerthika Cherala </h1>");
+    await httpContext.Response.WriteAsync("<h1> Shiva Krishna Beeraboina </h1>");
 
     // http Request Methods
 
@@ -35,16 +39,61 @@ app.Run(async (HttpContext httpContext) =>
         await httpContext.Response.WriteAsync($"<p>{key} : {value}</p>");
     }
 
-    Stream requestBody = httpContext.Request.Body;
-    using (StreamReader reader = new StreamReader(requestBody, Encoding.UTF8))
+    // Get Route Values
+    RouteValueDictionary routeValues = httpContext.Request.RouteValues;
+    foreach (var kvp in routeValues)
     {
-        string body = await reader.ReadToEndAsync();
-        await httpContext.Response.WriteAsync($"<p>Request Body: {body}</p>");
+        var key = kvp.Key;
+        var value = kvp.Value;
+        await httpContext.Response.WriteAsync($"<p>{key} : {value}</p>");
     }
 
+    // Query String - GET Request
+    if (httpContext.Request.Method == "GET")
+    {
+        if (httpContext.Request.Query.ContainsKey("id"))
+        {
+            string id = httpContext.Request.Query["id"];
+            await httpContext.Response.WriteAsync($"<p>Id: {id}</p>");
+        }
+    }
+
+    if (httpContext.Request.Method == "POST")
+    {
+        StreamReader reader = new StreamReader(httpContext.Request.Body);
+        string body = await reader.ReadToEndAsync();
+
+        // Read Query Parameters --  You will get request in string ; need to convert to key-value pairs
+        // There might be chance that there can be multiple values for same key like age=20&age=30 so for that we use StringValues data type
+        Dictionary<string, StringValues> queryDict =  Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery( body );
+        foreach (string key in queryDict.Keys)
+        {
+            StringValues values = queryDict[key];
+            await httpContext.Response.WriteAsync($"<p>{values.ToString()}</p>");
+        }
+
+        if (queryDict.ContainsKey("firstName"))
+        {
+            StringValues firstNames = queryDict["firstName"];
+            // get only first value of first name
+            string firstValueOfFirstName = firstNames[0];
+            await httpContext.Response.WriteAsync($"<p>First Value of First Name: {firstValueOfFirstName}</p>");
+
+            // get all values of first name
+            foreach (string firstName in firstNames)
+            {
+                await httpContext.Response.WriteAsync($"<p>First Name: {firstName}</p>");
+            }
+        }
+
+
+
+    }
 
 });
-
+*/
 //app.MapGet("/", () => "Hello World!");
+
+CalculatorOperationsRequests.Caluclator(app);
 
 app.Run();
